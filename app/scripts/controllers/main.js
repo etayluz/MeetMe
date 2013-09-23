a641450f1ed90922791f6ba7e05c5f71d6933b9b
Etay Luz <etayluz@gmail.com>
2013-09-23 14:27:30 -0400

    Stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,24 +2,25 @@ angular.module('yoAngularApp')
   .controller('MainCtrl', function ($scope) {
 
     $scope.appointmentList = [];
-    $scope.newAppointment = {
-      title : "defaultTitle",
-      start_date : "2013-09-09",
-      end_date : "2013-09-09",
-      appointment_with : "Dr. John",
-      description: "dentist appointment"
-    };
 
     $scope.addAppointment = function(newAppointment) {
-      var appointment = angular.copy(newAppointment);
+      var appointment = {
+        title : "Default",
+        start_date : "2013-09-09",
+        end_date : "2013-09-09",
+        appointment_with : "Default",
+        description: "Default"
+      };
+
       $scope.appointmentList.push(appointment);
+      $scope.editAppointment($scope.appointmentList.length - 1);
     };
 
     $scope.deleteAppointment = function(appointmentIndex) {
       $scope.appointmentList.splice(appointmentIndex,1);
     }
 
-     $scope.toggleEdit = function(appointmentIndex) {
+     $scope.editAppointment = function(appointmentIndex) {
       $scope.editing = !$scope.editing;
       $scope.appointmentIndex = appointmentIndex;
     }   
